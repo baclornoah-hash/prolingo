4900ae6629ebd898c5be52cf9a7242e69903f7b6
baclornoah-hash <baclornoah@gmail.com>
2026-09-07 12:12:43 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,34 +1,23 @@
-// firebase-config.js
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
 import {
   getAuth
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
 import {
   getFirestore
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-import {
-  getStorage
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBGWM-acjKpP1qjW7MBEUAI-Tls7tP_Rk",
+  apiKey: "REAL_API_KEY_FROM_FIREBASE",
   authDomain: "prolingo-2de9d.firebaseapp.com",
   projectId: "prolingo-2de9d",
-  storageBucket: "prolingo-2de9d.firebasestorage.app",
-  messagingSenderId: "59292786878",
-  appId: "1:59292786878:web:bd6e737458fdd8d9aabeef"
+  storageBucket: "REAL_STORAGE_BUCKET",
+  messagingSenderId: "REAL_MESSAGING_SENDER_ID",
+  appId: "REAL_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
 
-const auth = getAuth(app);
-const db = getFirestore(app);
-const storage = getStorage(app);
-
-export {
-  app,
-  auth,
-  db,
-  storage
-};
+export const auth = getAuth(app);
+export const db = getFirestore(app);
