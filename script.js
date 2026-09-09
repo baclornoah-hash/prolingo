6832649b934a1a1b1f641c8f5c2ee7641637817c
baclornoah-hash <baclornoah@gmail.com>
2026-09-09 12:47:01 +0800

    Update script.js

--- script.js
+++ script.js
@@ -96,13 +96,6 @@ function showView(name) {
 
   const targetView = $(viewId);
 
-  alert(
-  "name = " + name +
-  "\nviewId = " + viewId +
-  "\ntargetView = " + !!targetView +
-  "\nviews[name] = " + !!views[name]
-);
-
   if (!targetView || !views[name]) {
     console.error(`View not found: view-${name}`);
     return;
